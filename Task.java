842a14b4d9985fd1353e33793519eeb1b929abae
sachinchandra1596 <sachinchandra1509@gmail.com>
2025-09-03 16:44:36 +0530

    Create Task.java

---
+++ Task.java
@@ -0,0 +1,18 @@
+package com.example.taskservice.model;
+
+public;
+    private String title;
+
+    public Task() {}
+
+    public Task(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+}
